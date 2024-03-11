3a6fd4f6cda260dd36452862930fac851f3c54dc
vduplexy <timba.gc.1784@gmail.com>
2024-03-11 22:23:22 +0800

    Fixed errors when running yarn build. Added escaped entities  apostrophe in the store name Angelo's by making it to Angelo&apos;s. Removed console.log(total) from UseEffect in Price

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       </div>
       {/* LOGO */}
       <div className="text-xl md:font-bold flex-1 md:text-center">
-        <Link href="/">ANGELO'S</Link>
+        <Link href="/">ANGELOS</Link>
       </div>
       {/* MOBILE MENU */}
       <div className="md:hidden">
